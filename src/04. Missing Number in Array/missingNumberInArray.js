ea9f63959119419458884e2e6d93ca81b23cf2c5
Tushar Roy <binarydevelop@gmail.com>
2022-05-13 11:17:40 +0530

    add: missingNumberInArray

---
+++ src/04. Missing Number in Array/missingNumberInArray.js
@@ -0,0 +1,16 @@
+function missingNumber(array, n){
+    array.sort((a , b) => {
+      return a - b ; 
+    })
+    for(let i = 0; i < array.length + 1 ;i++){ 
+      if(array[i] !== i+1){
+        return (i+1);
+      }
+    }
+}
+
+/* 
+array.length + 1 to compensate for the case when N = 2 or array length is 2 and we have a
+single item in array [1] so the index of it is 0 and the second element is missing. 
+
+*/ 
